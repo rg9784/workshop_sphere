7d2813bbca401c8aae24362856461244b20294a6
Vishwa Sriram <43082361+techsparkling@users.noreply.github.com>
2023-08-21 23:11:01 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-// App.js
 import React from "react";
-import Routes from "./Routes";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Note the use of "Routes"
 
+import ProjectPage from "./scenes/ProjectPage";
+import AdminLogin from "./scenes/AdminLogin";
+import AdminRegister from "./scenes/AdminRegister";
+import AdminPanel from "./scenes/AdminPanel";
+import Homepage from "./scenes/Homepage";
+// Import other components for different pages
 
 function App() {
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/project-page" element={<ProjectPage />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin-register" element={<AdminRegister />} />
+        <Route path="/admin-panel" element={<AdminPanel />} />
+        <Route path="/" element={<Homepage />} />
+
+        
+      </Routes>
+    </Router>
   );
 }
 
